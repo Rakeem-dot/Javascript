6ba0ef44f772389499797a5d5292d39b5864bdbf
Rakeem Broomes <rrbroomes313@gmail.com>
2025-07-08 15:10:00 +0000

    Calculating Fahrenheit

---
+++ Fahrenheit.js
@@ -0,0 +1,10 @@
+//Declaring the variables
+
+const kelvin = 639;
+let celsius = kelvin - 273;
+let fahrenheit = celsius * (9/5) + 23;
+let newton = celsius * (33/100);
+
+
+//OUTPUT
+console.log(`The temperature is ${Math.floor(fahrenheit)} degrees fahrenheit`);
