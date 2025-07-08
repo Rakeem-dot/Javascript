182788b9b6784def449d69e30c8fdf35617110af
Rakeem Broomes <rrbroomes313@gmail.com>
2025-07-08 14:57:23 +0000

    My first javascript

---
+++ Main.js
@@ -0,0 +1,13 @@
+//Assigning Values
+let myAge = 21;
+let earlyYears = 2;
+let laterYears = myAge - 2;
+const myName = 'Rakeem BROOMES'.toLowerCase();
+
+//Declare new values
+laterYears = laterYears * 4;
+earlyYears = earlyYears * 10.5;
+let myAgeInDogYears = earlyYears + laterYears; //Added variables together to form 1
+
+//OUUTPUT
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
